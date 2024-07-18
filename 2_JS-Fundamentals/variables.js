8d6182254e4162fba7db0d61ba1d0d92e8c4002e
Gourav Kumar Dhiman <gourav.dhiman331@gmail.com>
2024-07-19 00:07:55 +0530

    Learn Variable Declaration & Definition

---
+++ 2_JS-Fundamentals/variables.js
@@ -0,0 +1,30 @@
+console.log('JS Fundamentals!!');
+
+
+// let
+let age = 10;
+console.log(age);
+
+age = "ten";
+console.log(age);
+
+
+// const,var,let
+let firstChildAge = 10;
+
+let first_child_age = 10;
+
+
+let age1;//declararion - referes to creating a variable
+age1 = 20;//definition - refers to assigning a value to the variable
+
+
+// const used which is constant like- dob, father name etc
+// const will not allowed us to define the value later
+const pi = 3.14;
+
+// var let us change the value later on
+var myName = 'Gourav';
+myName = 'Dhiman';
+console.log(myName);
+
