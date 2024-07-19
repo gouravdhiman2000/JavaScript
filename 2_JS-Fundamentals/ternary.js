f5919e5bfb1350f1ab850d66187f7d0cb737b0ab
Gourav Kumar Dhiman <gourav.dhiman331@gmail.com>
2024-07-19 16:15:34 +0530

    ternary operator in JS

---
+++ 2_JS-Fundamentals/ternary.js
@@ -0,0 +1,7 @@
+/**Ternary Operator */
+// Condition ? (True) Expression : (False) Expression
+let num = prompt("Enter the Number ");
+
+num %2 == 0 
+    ? console.log("Even") 
+    : console.log("Odd");
