f23e293a8135f0a182a0ca1bdcfb2e939a3d85f7
Gourav Kumar Dhiman <gourav.dhiman331@gmail.com>
2024-07-24 18:59:49 +0530

    for loop

---
+++ 3_Loops,Arrays.Functions&Objects/1_Loops/1_forLoop.js
@@ -0,0 +1,23 @@
+/** For Loop */
+// for(Initialize; Condition; Operation){//Body};
+// for(;;){//Body};
+
+// printing odd no.s upto 50
+// for(let i = 1; i <=50; i = i+2 ){
+//     console.log(i);
+// }
+
+for (let i=0;i<5;){
+    console.log(i);
+    i++;
+}
+
+let sum = 0;
+for (let i = 0; i <= 10; i+=2) {
+    sum += i;
+}
+console.log(sum);
+
+
+
+
