85017d88c42f41e2a2e7ee6a4daa030cff8426a4
Gourav Kumar Dhiman <gourav.dhiman331@gmail.com>
2024-07-24 19:05:57 +0530

    Function creation in JS

---
+++ 3_Loops,Arrays.Functions&Objects/2_Functions/1_function.js
@@ -0,0 +1,18 @@
+/** Functions in JavaScript */
+
+// function without parameter
+function greetUser(){
+    console.log("Hello User, Welcome to the Session");
+}
+
+// calling the function
+greetUser();
+
+
+// function with parameter
+function greetUsers(username){
+    console.log("Hello " + username + ", Welcome to the Session");
+}
+
+greetUsers('Gouravkumardhiman');
+greetUsers('Keshav');
