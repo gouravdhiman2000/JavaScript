4aade3b19f3d3fe4ba93a7d3c6d5e3dd61c1b4f4
Gourav Kumar Dhiman <gourav.dhiman331@gmail.com>
2024-07-24 19:24:20 +0530

    Set Data Structure in JS

---
+++ 3_Loops,Arrays.Functions&Objects/3_Arrays/8_setDataStructure.js
@@ -0,0 +1,16 @@
+const mySet = new Set();
+
+mySet.add(1);
+mySet.add(2);
+mySet.add(3);
+mySet.add(2); //ignored , already exits
+
+console.log(mySet.size);// 3
+console.log(mySet.has(2)); //true
+
+
+mySet.delete(3);
+console.log(mySet.size); // 2
+
+mySet.clear();
+console.log(mySet.size); // 0
